09c2697c39fbbe036bc7d088159845d64103b95d
Heriberto Carreras <heriberto.carreras.devariel@gmail.com>
2024-06-17 13:48:28 -0400

    4-puts.c

---
+++ hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main() {
+    /* Use puts function to print the string*/
+    
+	   puts("Programming is like building a multilingual puzzle");
+    
+    
+    return 0;
+}
+
