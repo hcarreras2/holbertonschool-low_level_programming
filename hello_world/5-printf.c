53d5f13de86b23c6ee1c16f679f8e00ca5eb71b0
Heriberto Carreras <heriberto.carreras.devariel@gmail.com>
2024-06-17 14:16:25 -0400

    5-printf.c

--- hello_world/5-printf.c
+++ hello_world/5-printf.c
@@ -1,12 +1,13 @@
-/*
- * File: 4-puts.c
+#include <stdio.h>
+/**
+ *
  * Auth: Heriberto Carreras Devariel <9473@holbertonschool.com>
  *
- **/
-#include <stdio.h>
+ */
 
 int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n")
+
 			return (0);
 			}
