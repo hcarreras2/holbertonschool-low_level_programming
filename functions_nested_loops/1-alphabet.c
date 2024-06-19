3b7cf502f6e89cbe3c6e614c01a3f9392b3bbec9
Heriberto Carreras <heriberto.carreras.devariel@gmail.com>
2024-06-19 12:58:03 -0400

    1-alphabet.c

---
+++ functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	char letter = 'a';
+
+	while (letter <= 'z')
+	{
+    _putchar(letter);
+	letter++;
+	}
+    return (0);
+}
