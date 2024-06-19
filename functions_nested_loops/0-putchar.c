90d90dbd17dc27018e8553b7fa11de6ff8ef594d
Heriberto Carreras <heriberto.carreras.devariel@gmail.com>
2024-06-19 11:00:03 -0400

    0-putchar.c

---
+++ functions_nested_loops/0-putchar.c
@@ -0,0 +1,22 @@
+#include <main.h>
+#include <stdio.h>
+
+/**
+ * Write a program that prints _putchar, followed by a new line.
+ *
+ * Return success (0)
+ */
+int main()
+{
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+
+	return 0;
+}
