8eb1fd986e35c16c8f3d02ba61730d0d7023d1a2
Heriberto Carreras <heriberto.carreras.devariel@gmail.com>
2024-06-19 21:44:54 -0400

    1-alphabet.c

--- functions_nested_loops/1-alphabet.c
+++ functions_nested_loops/1-alphabet.c
@@ -5,14 +5,12 @@
  *
  * Return: Always 0.
  */
-int main(void)
+void print_alphabet(void)
 {
-	char letter = 'a';
+	char letter;
 
-	while (letter <= 'z')
-	
-	_putchar(letter);
-	letter++;
+	for (letter= 'a'; letter <= 'z'; letter++)
+		_putchar(letter);
 	
 	_putchar('\n');
 
