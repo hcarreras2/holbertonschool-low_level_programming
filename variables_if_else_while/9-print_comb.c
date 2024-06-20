fb706f93ef5e96adf9a8cebb5b704bddf17b934b
Heriberto Carreras <heriberto.carreras.devariel@gmail.com>
2024-06-20 12:58:03 -0400

    1

--- variables_if_else_while/9-print_comb.c
+++ variables_if_else_while/9-print_comb.c
@@ -8,21 +8,20 @@
  */
 int main(void)
 {
-    int num;
+	int num;
 
-    for (num = 0; num <= 9; num++)
-    {
-        putchar(num + '0');
-
-        if (num != 9)
-        {
-            putchar(',');
-            putchar(' ');
-        }
+	for (num = 0; num <= 9; num++)
+	{
+		putchar(num + '0');
+		if (num != 9)
+		{
+			putchar(',');
+			putchar(' ');
+		}
     }
 
 	putchar('\n');
 
-    return (0);
+	return (0);
 }
 
