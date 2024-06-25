3794d6be62d27d270852ac9b8d6b7e93e075c44a
Heriberto Carreras <heriberto.carreras.devariel@gmail.com>
2024-06-24 22:58:44 -0400

    :3-puts

---
+++ pointers_arrays_strings/3-puts.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/*
+ *_puts - Prints the string to stdout.
+ *@str: The string that is going to be printed.
+ */
+void _puts(char *str)
+{
+	int s = 0;
+	while (str[s])
+	{
+		_putchar(str[s]);
+		++s;
+	}
+	_putchar('\n');
+}
