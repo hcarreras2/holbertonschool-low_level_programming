d18d43cd8b75f3f93b78dea2b188eca60ede0b17
Heriberto Carreras <heriberto.carreras.devariel@gmail.com>
2024-06-24 23:33:22 -0400

    7-puts_half.c

---
+++ pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+ * puts_half - Prints half of a string, followed by a new line
+ * @str: String to be printed
+ */
+void puts_half(char *str)
+{
+    int len = 0, i, n;
+
+    while (str[len] != '\0')
+    {
+        len++;
+    }
+
+    if (len % 2 == 0)
+    {
+        n = len / 2;
+    }
+    else
+    {
+        n = (len - 1) / 2 + 1;
+    }
+
+    for (i = n; i < len; i++)
+    {
+        _putchar(str[i]);
+    }
+
+    _putchar('\n');
+}
+
