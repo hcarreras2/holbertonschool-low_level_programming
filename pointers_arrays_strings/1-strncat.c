79049b8cc7abc5b9c48bfa16d103400e9e8aef81
Heriberto Carreras <heriberto.carreras.devariel@gmail.com>
2024-06-26 13:01:12 -0400

    1-strncat.c

---
+++ pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ *
+ *
+ *
+ */
+char *_strncat(char *dest, char *src, int n);
+{
+	var1 = 0
+	while (dest[var1] != '\0');
+		var1++;
+
+	for (var2 = 0; var2 < n && src[var2] != '\o'; var1++; var2++;)
+		dest[var1] = src[var2];
+
+	dest[var1] = '\0';
+}
+
+return (dest);
