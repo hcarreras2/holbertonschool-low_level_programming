7907b82f66ceffcf018841dda4522befdbb8882f
Heriberto Carreras <heriberto.carreras.devariel@gmail.com>
2024-06-26 13:25:08 -0400

    1-strncat.c

--- pointers_arrays_strings/1-strncat.c
+++ pointers_arrays_strings/1-strncat.c
@@ -1,19 +1,22 @@
 #include "main.h"
+#include <stdio.h>
 /**
  *
  *
  *
  */
-char *_strncat(char *dest, char *src, int n);
+char *_strncat(char *dest, char *src, int n)
 {
-	var1 = 0
-	while (dest[var1] != '\0');
+	int var1 = 0
+	int var2;
+
+	while (dest[var1] != '\0')
 		var1++;
 
-	for (var2 = 0; var2 < n && src[var2] != '\o'; var1++; var2++;)
+	for (var2 = 0; var2 < n && src[var2] != '\0'; var1++, var2++)
 		dest[var1] = src[var2];
 
 	dest[var1] = '\0';
-}
 
-return (dest);
+	return dest;
+}
