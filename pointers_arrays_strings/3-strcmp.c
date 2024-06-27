2f697546d9dbe1e58fb6cb9e5bb8a66080c975ef
Heriberto Carreras <heriberto.carreras.devariel@gmail.com>
2024-06-27 10:22:26 -0400

    3-strcmp.c

--- pointers_arrays_strings/3-strcmp.c
+++ pointers_arrays_strings/3-strcmp.c
@@ -1,7 +1,11 @@
 #include "main.h"
 /**
+ * _strcmp - Compares two strings.
+ * @s1: The first string to be compared.
+ * @s2: The second string to be compared.
  *
- *
+ * Return: A negative value if s1 is less than s2, 0 if s1 equals s2,
+ *			and a positive value if s1 is greater than s2.
  */
 int _strcmp(char *s1, char *s2)
 {
