2cda77b3ce173919fabc0865614ddff9ce51030b
Heriberto Carreras <heriberto.carreras.devariel@gmail.com>
2024-06-27 13:19:38 -0400

    01-main.c

--- pointers_arrays_strings/0-memset.c
+++ pointers_arrays_strings/0-memset.c
@@ -6,7 +6,7 @@
  */
 char *_memset(char *s, char b, unsigned int n)
 {
-	int i = 0;
+	unsigned int i = 0;
 
 	for (i = 0; i <= n; i++)
 		s[i] = b;
--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -17,3 +17,4 @@ void reverse_array(int *a, int n);
 char *string_toupper(char *);
 char *cap_string(char *);
 char *_strcat(char *dest, char *src);
+char *_memset(char *s, char b, unsigned int n);
