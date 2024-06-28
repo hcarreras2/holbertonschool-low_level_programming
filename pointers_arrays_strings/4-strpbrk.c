80640e8dbd8ea47dbcda5f2cb8d0f71b252ee73b
Heriberto Carreras <heriberto.carreras.devariel@gmail.com>
2024-06-27 22:37:37 -0400

    4444

---
+++ pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ *
+ *
+ *
+ */
+char *_strpbrk(char *s, char *accept)
+{
+	char *a;
+
+	while (*s)
+	{
+		for (a = accept; *a; a++)
+		{
+			if (s* == *a)
+				return (s);
+		}
+		s++;
+	}
+	return (NULL);
+}
