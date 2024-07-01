b6d3b6fc227b49d7997c81f5749f76762e9eb3d6
Heriberto Carreras <heriberto.carreras.devariel@gmail.com>
2024-07-01 14:52:37 -0400

    1-print_rev_recursion

--- recursion/1-print_rev_recursion.c
+++ recursion/1-print_rev_recursion.c
@@ -1,16 +1,14 @@
 #include "main.h"
 
 /**
- *
- *
+ * _print_rev_recursion - Prints a string in reverse using recursion
+ * @s: The string to be printed
  */
 void _print_rev_recursion(char *s)
 {
 	if (*s == '\0')
-	{
-		_putchar('\n');
-			return;
-	}
+		return;
+
+	_print_rev_recursion(s + 1);
 	_putchar(*s);
-	_print_rev_recursion(s - 1);
 }
