66c3d9c76f7c4b93f18a21d695a7b81cb4cd0e4f
Heriberto Carreras <heriberto.carreras.devariel@gmail.com>
2024-07-02 21:16:37 -0400

    0-whatsmyname.c

---
+++ argc_argv/README.md
@@ -0,0 +1,1 @@
+In this project I will be creating programs using the argc_argv prototypes.
---
+++ argc_argv/main.h
@@ -0,0 +1,3 @@
+int _putchar(char c);
+int main(int argc __attribute__((unused)), char *argv[]);
+int _atoi(char *s);
