7de1a6aaf989fbc43d4a167a067349528086ac1e
Heriberto Carreras <heriberto.carreras.devariel@gmail.com>
2024-07-04 22:07:39 -0400

    1-string_nconcat

--- more_malloc_free/1-string_nconcat.c
+++ more_malloc_free/1-string_nconcat.c
@@ -13,8 +13,8 @@
  */
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
+	unsigned int len1, len2, i, j;
 	char *concat;
-	unsigned int len1, len2;
 
 	if (s1 == NULL)
 		s1 = "";
@@ -32,8 +32,13 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	if (concat == NULL)
 		return (NULL);
 
-	strcpy(concat, s1);
-	strncat(concat, s2, n);
+	for (i = 0; i < len1; i++)
+		concat[i] = s1[i];
+
+	for (j = 0; j < n; j++)
+		concat[i + j] = s2[j];
+
+	concat[i + j] = '\0';
 
 	return (concat);
 }
