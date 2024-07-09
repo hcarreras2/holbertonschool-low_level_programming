92ced046f261bbe28c4e972f26edfc71baa8a39a
Heriberto Carreras <heriberto.carreras.devariel@gmail.com>
2024-07-09 14:34:49 -0400

    0-main.c

---
+++ structures_typedef/dog.h
@@ -0,0 +1,16 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - The characteristics of dog
+ * @name: name of dog
+ * @age: the age of dog
+ * @owner: name of the owner
+ *
+ */
+struct dog
+{
+  char *name;
+  float age;
+  char *owner;
+};
