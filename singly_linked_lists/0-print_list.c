759adc38a2802d3d6d5a8ec44f35fef08da2fa2f
Heriberto Carreras <heriberto.carreras.devariel@gmail.com>
2024-07-27 12:18:02 -0400

    betty

--- singly_linked_lists/0-print_list.c
+++ singly_linked_lists/0-print_list.c
@@ -3,8 +3,11 @@
 #include <stdio.h>
 #include "lists.h"
 /**
-*
-*
+ * print_list - Prints all the elements of a list_t list.
+ *
+ * @h: The head of the list_t list.
+ *
+ * Return: The number of nodes in the list.
 */
 size_t print_list(const list_t *h)
 {
