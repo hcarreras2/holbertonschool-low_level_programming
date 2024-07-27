d5bf4ba246a52289da079b8a78a1a0889db31452
Heriberto Carreras <heriberto.carreras.devariel@gmail.com>
2024-07-27 12:30:21 -0400

    betty 2

--- singly_linked_lists/lists.h
+++ singly_linked_lists/lists.h
@@ -1,31 +1,33 @@
-#include <stdio.h>
+#ifndef LISTS_H
+#define LISTS_H
 
 /**
-
- * struct list_s - singly linked list
-
- * @str: string - (malloc'ed string)
-
- * @len: length of the string
-
- * @next: points to the next node
-
- *
-
- * Description: singly linked list node structure
-
- */
+* struct list_s - singly linked list
+* @str: string - (malloc'ed string)
+* @len: length of the string
+* @next: points to the next node
+*
+* Description: singly linked list node structure
+*/
 typedef struct list_s
 {
 
-    char *str;
+	char *str;
 
-    unsigned int len;
+	unsigned int len;
 
-    struct list_s *next;
+	struct list_s *next;
 
 } list_t;
+/**
+* print_list - Prints all the elements of a list_t list.
+*
+* @h: The head of the list_t list.
+*
+* Return: The number of nodes in the list.
+*/
 
 size_t print_list(const list_t *h);
 int _putchar(char c);
 
+#endif
