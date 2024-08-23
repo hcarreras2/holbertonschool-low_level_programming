c582c58cecea1c5c5243366b7bf2fbdfdd3c137f
Heriberto Carreras <heriberto.carreras.devariel@gmail.com>
2024-08-22 22:32:34 -0400

    1

---
+++ malloc_free/3-alloc_grid.c
@@ -0,0 +1,46 @@
+#include "main.h"
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ *alloc_grid- gets users input
+ *@width: stores first int
+ *@height: stores second input
+ *Return: results
+ */
+
+int **alloc_grid(int width, int height)
+{
+	int **grid;
+	int i, j;
+
+	grid = (int **)malloc(height * sizeof(int *));
+	if (width <= 0 || height <= 0)
+		return (NULL);
+
+	if (grid == NULL)
+	{
+		return (NULL);
+	}
+
+	for (i = 0; i < height; i++)
+	{
+		grid[i] = (int *)malloc(width * sizeof(int));
+		if (grid[i] == NULL)
+		{
+			for (j = 0; j < i; j++)
+			{
+				free(grid[j]);
+			}
+			free(grid);
+			return (NULL);
+		}
+
+		for (j = 0; j < width; j++)
+		{
+			grid[i][j] = 0;
+		}
+	}
+
+	return (grid);
+}
